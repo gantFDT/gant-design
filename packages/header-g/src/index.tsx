5ad6eb56ac0c8a94238c02073155d27b14fd6e35
Solin.M <solin.m@MacBook-Pro.local>
2020-02-20 14:56:15 +0800

    2

--- packages/header-g/src/index.tsx
+++ packages/header-g/src/index.tsx
@@ -1,7 +1,7 @@
 import './index.less';
 import React, { ReactNode, useRef, useState, useEffect, useMemo, useCallback } from 'react';
 import classnames from 'classnames'
-import { Dropdown, Menu, Button, Icon } from 'antd'
+import { Dropdown, Menu, Button, Icon } from 'antd';
 import ReactResizeDetector from 'react-resize-detector';
 import _ from 'lodash';
 
