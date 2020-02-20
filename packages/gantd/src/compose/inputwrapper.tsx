15d6eabbb84f772841fbff0f7e7ceeae65d4cb54
袁官东 <yuangd@gantsoftware.com>
2020-02-20 11:49:47 +0800

    update

--- packages/gantd/src/compose/inputwrapper.tsx
+++ packages/gantd/src/compose/inputwrapper.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+// import classnames from 'classnames'
+
+// export default WrapperedComponent => React.forwardRef<HTMLDivElement, any>((props, ref) => {
+//   const factory = React.createFactory(WrapperedComponent)
+//   const {style,className,...restProps} = props
+//   return (
+//     <div className={classnames('gant-input',className)} style={{...style}} ref={ref}>{factory(restProps)}</div>
+//   )
+// })
+
 import classnames from 'classnames'
 
 export default WrapperedComponent => React.forwardRef<HTMLDivElement, any>((props, ref) => {
   const factory = React.createFactory(WrapperedComponent)
-  const {style,className,...restProps} = props
   return (
-    <div className={classnames('gant-input',className)} style={{...style}} ref={ref}>{factory(restProps)}</div>
+    <div className={classnames('gant-input')} ref={ref}>{factory(props)}</div>
   )
 })
