383b5dc5bd502f8704362984af132477bee11a1b
袁官东 <yuangd@gantsoftware.com>
2020-02-21 14:26:43 +0800

    网址

--- stories/inputurl/code.js
+++ stories/inputurl/code.js
@@ -1,13 +1,9 @@
-
-import { InputUrl,SwitchStatus } from '@packages/gantd/src'
-import React, { useState } from 'react';
-import { Button } from 'antd'
-import codeList from './code'
-import CodeDecorator from '../_util/CodeDecorator';
-
-
-const cmps = [
-  () => {
+export default [
+  `
+  import React, { useState } from 'react';
+  import {InputCellPhone} from 'gantd';
+  
+  const Demo = () => {
     const [value, setValue] = useState('https://www.npmjs.com')
     const onSave = (id, value, cb) => {
       console.log(id, value);
@@ -17,15 +13,46 @@ const cmps = [
       <InputUrl placeholder='不可编辑' allowEdit={false}  value="https://www.npmjs.com"/>
       <InputUrl placeholder='可编辑' allowEdit={true} onSave={onSave} value={value} onChange={setValue} />
     </>
-  },
-  () => {
+  }
+  
+  ReactDOM.render(
+      <Demo />,
+      mountNode,
+  );
+  `,
+  
+  
+  
+  
+  
+  
+  
+  `
+  import React, { useState } from 'react';
+  import {InputCellPhone, SwitchStatus} from 'gantd';
+  
+  const Demo = () => {
     const [edit, setEdit] = useState('CANCEL')
     return <>
       <Button onClick={() => setEdit(SwitchStatus)} style={{ marginBottom: 5 }} size="small">{!(edit === 'EDIT') ? '进入编辑' : '退出编辑'}</Button>
       <InputUrl placeholder='请输入' edit={edit} style={{ margin: '5px 0' }} />
     </>
-  },
-  () => {
+  }
+  
+  ReactDOM.render(
+      <Demo />,
+      mountNode,
+  );
+  `,
+    
+  
+  
+  
+  `
+  import React, { useState } from 'react';
+  import {InputCellPhone} from 'gantd';
+  
+  const Demo = () => {
     const [value, setValue] = useState('https://www.npmjs.com')
     const onSave = (id, value, cb) => {
       console.log(id, value);
@@ -35,32 +62,17 @@ const cmps = [
       <InputUrl placeholder='网址校验' allowEdit={true} value={value} onSave={onSave} onChange={setValue} />
     </>
   }
-]
-
-
-const config = {
-  useage: `<b>🖍 读写分离</b></br>
-    <b>📱 邮箱校验</b>
+  
+  ReactDOM.render(
+      <Demo />,
+      mountNode,
+  );
   `,
-  codes: codeList,
-  inline: true,
-  children: [
-    {
-      title: '是否可编辑',
-      describe: '在后面展示一个编辑按钮，通过修改allowEdit参数控制是否可以编辑，allowEdit默认true',
-      cmp: cmps[0]
-    },
-    {
-      title: '编辑状态受控',
-      describe: '受其他组件控制展示的形态',
-      cmp: cmps[1]
-    },
-    {
-      title: '校验',
-      describe: '如果不符合网址格式，则退化为纯文本',
-      cmp: cmps[2]
-    }
+    
+  
+  
   ]
-}
-
-export default () => <CodeDecorator config={config} />
+  
+  
+  
+  
