1deebb29b78ca1b2aad62a6f1f96bafdd746cf05
chenyl <chenyl@gantsoftware.com>
2020-02-27 17:46:35 +0800

    feat 更新语言输入框

--- stories/inputlanguage/code.js
+++ stories/inputlanguage/code.js
@@ -4,26 +4,17 @@ export default [
   import {InputLanguage} from 'gantd';
   
   const Demo = () => {
-    const localeList = [
+    const [value, setValue] = useState([
       {
-        locale: 'zh-CN',
-        label: '中文',
-        value: ''
-      },
-      {
-        locale: 'en-US',
-        label: '英文',
-        value: ''
+        value: "中文文本",
+        locale: 'zh-CN'
       }
-    ]
-    const [value, setValue] = useState({ locale: 'zh-CN', value: '中文' })
+    ])
     const onSave = (id, value, cb) => {
-      console.log(id, value);
       cb()
     }
     return <>
-      <InputLanguage localeList={localeList} placeholder='不可编辑' allowEdit={false} value={{ locale: 'zh-CN', value: '中文' }} />
-      <InputLanguage localeList={localeList} placeholder='可编辑' allowEdit={true} onSave={onSave} value={value} onChange={setValue} />
+      <InputLanguage placeholder='输入当前语言文本' allowEdit={true} onSave={onSave} value={value} onChange={setValue} />
     </>
   }
   
@@ -32,84 +23,7 @@ export default [
       mountNode,
   );
   `,
-  
-  
-  
-  
-  
-  
-  
-  `
-  import React, { useState } from 'react';
-  import {InputLanguage, SwitchStatus} from 'gantd';
-  
-  const Demo = () => {
-    const localeList = [
-      {
-        locale: 'zh-CN',
-        label: '中文',
-        value: ''
-      },
-      {
-        locale: 'en-US',
-        label: '英文',
-        value: ''
-      }
-    ]
-    const [edit, setEdit] = useState('CANCEL')
-    const [value, setValue] = useState({ locale: 'zh-CN', value: '你好' })
-    return <>
-      <Button onClick={() => setEdit(SwitchStatus)} style={{ marginBottom: 5 }} size="small">{!(edit === 'EDIT') ? '进入编辑' : '退出编辑'}</Button>
-      <InputLanguage localeList={localeList} placeholder='请输入' value={value} onChange={setValue} edit={edit} style={{ margin: '5px 0' }} />
-    </>
-  }
-  
-  ReactDOM.render(
-      <Demo />,
-      mountNode,
-  );
-  `,
-    
-  
-  
-  
-  `
-  import React, { useState } from 'react';
-  import {InputLanguage} from 'gantd';
-  
-  const Demo = () => {
-    const localeList = [
-      {
-        locale: 'zh-CN',
-        label: '中文',
-        value: ''
-      },
-      {
-        locale: 'en-US',
-        label: '英文',
-        value: ''
-      }
-    ]
-    const [value, setValue] = useState({ locale: 'en-US', value: 'Hello' })
-    const onSave = (id, value, cb) => {
-      console.log(id, value);
-      cb()
-    }
-    return <>
-      <InputLanguage localeList={localeList} placeholder='请输入' allowEdit={true} value={value} onSave={onSave} onChange={setValue} />
-    </>
-  }
-  
-  ReactDOM.render(
-      <Demo />,
-      mountNode,
-  );
-  `,
-    
-  
-  
-  ]
-  
-  
-  
-  
+]
+
+
+
