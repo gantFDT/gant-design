62347703ea3e4b8c053adebcccbe4496b1be3e22
张洋瑞 <zhangdousang@yeah.net>
2020-02-27 12:24:46 +0800

    modal-g fix

--- packages/modal-g/src/index.tsx
+++ packages/modal-g/src/index.tsx
@@ -1,41 +1,31 @@
-import React, { useContext, useEffect } from 'react';
-import ResizableModal from './ResizableModal';
-import ResizableProvider from './ResizableProvider';
-import ModalContext from './Context';
-const uuid = 'modal-g-uuid';
+import React, { useContext, useEffect } from 'react'
+import ResizableModal from './ResizableModal'
+import ResizableProvider from './ResizableProvider'
+import ModalContext from './Context'
+/// <reference path='types.d.ts' />
 
-const ContextContent = ({
-    id,
-    onSizeChange,
-    children
-}) => {
-    const { state: { modals } } = useContext(ModalContext);
-    const { width, height } = modals[id];
+const uuid = 'modal-g-uuid'
+
+const ContextContent = ({ id, onSizeChange, children }) => {
+    const { state: { modals } } = useContext(ModalContext)
+    const { width, height } = modals[id]
 
     useEffect(() => {
-        onSizeChange && onSizeChange(width, height);
-    }, [width, height]);
+        onSizeChange && onSizeChange(width, height)
+    }, [width, height])
 
     return <>{children}</>
 }
 
 const Modal = function (props) {
     const {
-        i18n,
         maxZIndex,
         minWidth,
         minHeight,
-        itemState,
-        title,
-        visible,
-        style,
-        classname,
-        canMaximize,
-        canResize,
         children,
         onSizeChange,
         ...restProps
-    } = props;
+    } = props
 
     return <ResizableProvider
         maxZIndex={maxZIndex}
@@ -44,13 +34,6 @@ const Modal = function (props) {
     >
         <ResizableModal
             id={uuid}
-            title={title}
-            wrapClassName={classname}
-            style={style}
-            itemState={itemState}
-            visible={visible}
-            canMaximize={canMaximize}
-            canResize={canResize}
             {...restProps}
         >
             <ContextContent id={uuid} onSizeChange={onSizeChange}>
@@ -64,5 +47,5 @@ Modal.defaultProps = {
     isModalDialog: true
 }
 
-export default Modal;
-export { ResizableModal, ResizableProvider, ModalContext };
+export default Modal
+export { ResizableModal, ResizableProvider, ModalContext }
