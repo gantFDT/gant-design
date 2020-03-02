2aacedc592971985576d59de9206a41fe39269ce
张洋瑞 <zhangdousang@yeah.net>
2020-03-02 18:40:36 +0800

    storybook hack （Canvas tab name 未hack成功）

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,9 +1,14 @@
 import '@storybook/addon-backgrounds/register';
-// import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-notes/register';
 import '@storybook/addon-console';
 import '@storybook/addon-viewport/register';
 import 'addon-contexts/register';
 import 'storybook-dark-mode/register';
+import addons from '@storybook/addons';
 // import '@storybook/addon-jest/register';
+// import '@storybook/addon-actions/register';
+
+setTimeout(function () {
+    addons.elements.tab["storybookjs/notes/panel"].title = "文档";
+}, 180);
--- stories/index.js
+++ stories/index.js
@@ -3,26 +3,26 @@ import { storiesOf } from '@storybook/react';
 import pages from './router.config.js'
 import { Tag } from 'antd'
 import './index.css'
-import favicon from '../assests/images/favicon.ico'
+// import favicon from '../assests/images/favicon.ico'
 
-const updateFavicon = (img) => {
-  var link = document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = img;
-	document.getElementsByTagName('head')[0].appendChild(link);
-}
-const updateTitle = (title) => {
-  document.title = title
-}
+// const updateFavicon = (img) => {
+// 	var link = document.createElement('link');
+// 	link.type = 'image/x-icon';
+// 	link.rel = 'shortcut icon';
+// 	link.href = img;
+// 	document.getElementsByTagName('head')[0].appendChild(link);
+// }
+// const updateTitle = (title) => {
+// 	document.title = title
+// }
 
-setTimeout(()=>{
-	const updateTitle = (title) => {
-		document.title = title
-	}
-	updateFavicon(favicon)
-	updateTitle('GantD | 专注于数据密集型业务场景React组件库')
-},200)
+// setTimeout(() => {
+// 	const updateTitle = (title) => {
+// 		document.title = title
+// 	}
+// 	updateFavicon(favicon)
+// 	updateTitle('GantD | 专注于数据密集型业务场景React组件库')
+// }, 200)
 
 pages.map(page => {
 	const storiresPage = storiesOf(page.name, module)
@@ -32,7 +32,7 @@ pages.map(page => {
 		let marked = ``
 		let config = {};
 		let title = `${item.name} ${item.nameZh} ${item.package ? '📦' : ''}`;
-		
+
 		try {
 			marked = require('./' + dirname + "/README.md");
 			config = require('./' + dirname + "/config.js");
@@ -44,7 +44,7 @@ pages.map(page => {
 				display: 'flex',
 				alignItems: 'center'
 			}}>
-				<div style={{ fontSize: '30px', color: '#000',fontWeight:'bold' }}>{title}</div>
+				<div style={{ fontSize: '30px', color: '#000', fontWeight: 'bold' }}>{title}</div>
 				{item.package && <Tag color="#108ee9" style={{ marginLeft: '10px' }}>可使用独立包</Tag>}
 				{item.package && <Tag style={{ marginLeft: '10px' }}>npm i {item.package} -S</Tag>}
 			</div>}
