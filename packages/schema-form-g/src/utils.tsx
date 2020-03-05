ba56ff4164f1c50ad9b987360616f505476dcf16
vveyZhang <vvey@vveys-MacBook-Pro.local>
2020-03-05 11:13:12 +0800

    fix(schemaForm): uiSchema逻辑优化

--- packages/schema-form-g/src/utils.tsx
+++ packages/schema-form-g/src/utils.tsx
@@ -114,7 +114,7 @@ export function getUIData(uiSchema: UISchema, type: "field" | "form", pathName?:
 			uiSchemaData = get(uiSchema, arr.slice(0, index + 1))
 		}
 	}
-	if (uiSchemaData == undefined) uiSchemaData = uiSchema;
+	if (uiSchemaData == undefined) uiSchemaData = { ...uiSchema };
 	uiArray.map(item => {
 		if (item.belong === type || item.belong.indexOf(type) >= 0) {
 			if (typeof item.name === "string") {
