6ca175642d456e20e0477117d781af8215040674
chenyl <chenyl@gantsoftware.com>
2020-03-06 13:58:32 +0800

    fix(icon): 修复iconfont图标的显示问题

--- packages/data-cell-g/src/icon/index.tsx
+++ packages/data-cell-g/src/icon/index.tsx
@@ -1,87 +1,99 @@
-import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react'
-import { Drawer, Radio, Empty } from 'antd'
-import { CustomIconOptions } from 'antd/lib/icon/IconFont'
-import classnames from 'classnames'
-import _ from 'lodash'
-import { compose, defaultProps, toClass, withProps } from 'recompose'
+import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react';
+import { Drawer, Radio, Empty } from 'antd';
+import { CustomIconOptions } from 'antd/lib/icon/IconFont';
+import classnames from 'classnames';
+import _ from 'lodash';
+import { compose, defaultProps, toClass, withProps } from 'recompose';
 
 import './index.less';
-import { Input, EditStatus, Group } from '..'
-import Icon, { IconComponent, IconProps } from './Icon'
-import { withEdit } from '../compose'
-import { WithEditInProps, WithEditOutProps } from '../compose/withEdit'
+import { Input, EditStatus, Group } from '..';
+import Icon, { IconComponent, IconProps } from './Icon';
+import { withEdit } from '../compose';
+import { WithEditInProps, WithEditOutProps } from '../compose/withEdit';
 
+const tr = a => a;
 
-const tr = a => a
-
-const { getOutLine, updateFromIconfontCN, Ant } = Icon
-const outline = getOutLine()
+const { getOutLine, updateFromIconfontCN, Ant } = Icon;
+const outline = getOutLine();
 
 const bodyStyle = {
   height: 'calc(100vh - 41px)',
   padding: 10,
-  overflow: 'hidden'
-}
+  overflow: 'hidden',
+};
 
 enum IconTypes {
   IconFont = 'IconFont',
-  AntIcon = 'AntIcon'
+  AntIcon = 'AntIcon',
 }
-
 
 const defaultprops = {
   perfix: 'icon-',
   // onChange(icon: string) { },
   value: '',
-}
+};
 
-type PropExtend<T, U> = U & {
-  [K in Exclude<keyof T, keyof U>]?: T[K]
-}
+type PropExtend<T, U> = U &
+  {
+    [K in Exclude<keyof T, keyof U>]?: T[K];
+  };
 
 type BasicProps = PropExtend<
   IconProps,
-  PropExtend<typeof defaultprops, {
-    onBlur?: Function,
-    [key: string]: any,
-  }>
->
+  PropExtend<
+    typeof defaultprops,
+    {
+      onBlur?: Function;
+      [key: string]: any;
+    }
+  >
+>;
 
-type IconHouseProps<T> = BasicProps & WithEditOutProps<T>
+type IconHouseProps<T> = BasicProps & WithEditOutProps<T>;
 
-type IconSelectorProps<T> = PropExtend<WithEditInProps<T>, BasicProps>
+type IconSelectorProps<T> = PropExtend<WithEditInProps<T>, BasicProps>;
 
-
-const IconHouse: React.FC<IconHouseProps<string>> = ({ onChange, value, onBlur = undefined, addonAfter, allowEdit, onEnter, ...props }) => {
+const IconHouse: React.FC<IconHouseProps<string>> = ({
+  onChange,
+  value,
+  onBlur = undefined,
+  addonAfter,
+  allowEdit,
+  onEnter,
+  perfix,
+  ...props
+}) => {
   const prefixCls = 'gant-icon-selector';
 
   // 图标抽屉
-  const [visible, setvisible] = useState(false)
+  const [visible, setvisible] = useState(false);
   // 图标id
-  const [IDs, setIDs] = useState([] as string[])
+  const [IDs, setIDs] = useState([] as string[]);
   // 搜索框
-  const [text, settext] = useState('')
+  const [text, settext] = useState('');
   // 当前icon
-  const [currentId, setCurrentId] = useState(value)
+  const [currentId, setCurrentId] = useState(value);
   // 当前显示的图标类型
 
   const iconTypes = useMemo(() => {
-    let iconTypeArr = [IconTypes.AntIcon]
+    let iconTypeArr = [IconTypes.AntIcon];
     if (!_.isEmpty(IDs)) {
-      iconTypeArr.push(IconTypes.IconFont)
+      iconTypeArr.push(IconTypes.IconFont);
     }
-    return iconTypeArr
-  }, [IDs])
+    return iconTypeArr;
+  }, [IDs]);
 
-  const [iconType, seticonType] = useState(iconTypes[0])
+  const [iconType, seticonType] = useState(iconTypes[0]);
 
-  const icons = useMemo(() => ({
-    IconFont: IDs,
-    AntIcon: outline
-  }), [IDs])
+  const icons = useMemo(
+    () => ({
+      IconFont: IDs,
+      AntIcon: outline,
+    }),
+    [IDs],
+  );
 
-
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const click = (e: MouseEvent) => {
@@ -89,76 +101,76 @@ const IconHouse: React.FC<IconHouseProps<string>> = ({ onChange, value, onBlur =
         // 点击了其他区域
         if (!ref.current.contains(e.target as Node)) {
           if (onBlur && typeof onBlur === 'function') {
-            onBlur()
+            onBlur();
           }
         }
       }
-
-    }
-    window.addEventListener('click', click, false)
-    return () => {
-      window.removeEventListener('click', click)
     };
-  }, [ref, onBlur])
+    window.addEventListener('click', click, false);
+    return () => {
+      window.removeEventListener('click', click);
+    };
+  }, [ref, onBlur]);
 
   useEffect(() => {
-    setCurrentId(value)
-  }, [value])
+    setCurrentId(value);
+  }, [value]);
 
-  const toggleVisible = useCallback(
-    () => {
-      if (!visible) { // 打开
-        settext('')
-      }
-      setvisible(!visible)
-    },
-    [visible],
-  )
+  const toggleVisible = useCallback(() => {
+    if (!visible) {
+      // 打开
+      settext('');
+    }
+    setvisible(!visible);
+  }, [visible]);
 
   const onSelectIcon = useCallback((id: string) => {
-    setCurrentId(id)
-    if (onChange) onChange(id)
-    toggleVisible()
-  }, [])
+    setCurrentId(id);
+    if (onChange) onChange(id);
+    toggleVisible();
+  }, []);
 
   // 获取图标id
   useState(() => {
     // const tag = queryIconHouse(iconWareHouse)
     const tag = document.querySelector('svg');
-    if (!tag) return
-    const iconIds = [].slice.call(tag.querySelectorAll('symbol')).map((symbol: Element) => symbol.id)
-    setIDs(iconIds)
-  })
+    if (!tag) return;
+    const iconIds = [].slice
+      .call(tag.querySelectorAll('symbol'))
+      .map((symbol: Element) => symbol.id);
+    setIDs(iconIds);
+  });
 
   // 切换图标
-  const handleTypeChange = useCallback(
-    (e) => {
-      seticonType(e.target.value)
-    },
-    [],
-  )
+  const handleTypeChange = useCallback(e => {
+    seticonType(e.target.value);
+  }, []);
 
-  const iconsWithFilter = useMemo(() => (
-    icons[iconType].filter(id => {
-      return id.includes(text)
-    })
-  ), [icons, iconType, text])
+  const iconsWithFilter = useMemo(
+    () =>
+      icons[iconType].filter(id => {
+        return id.includes(text);
+      }),
+    [icons, iconType, text],
+  );
 
   return (
     <div ref={ref} className="gant-icon-selector-wrapper">
       <Group gant>
         <div className="gant-icon-select" onClick={toggleVisible}>
-          {
-            currentId ? <Icon type={currentId} title={tr('点击切换')} {...props} /> : <span className={prefixCls + 'select-btn'}>{tr('点击选择')}</span>
-          }
+          {currentId ? (
+            <Icon type={currentId} perfix={perfix} title={tr('点击切换')} {...props} />
+          ) : (
+            <span className={prefixCls + 'select-btn'}>{tr('点击选择')}</span>
+          )}
         </div>
         {addonAfter ? <span className="ant-input-group-addon">{addonAfter}</span> : undefined}
       </Group>
       <Drawer
         width={visible ? 500 : 0}
-        title={tr("请选择图标")}
+        title={tr('请选择图标')}
         destroyOnClose
-        placement='right'
+        placement="right"
         onClose={toggleVisible}
         visible={visible}
         bodyStyle={bodyStyle}
@@ -166,69 +178,98 @@ const IconHouse: React.FC<IconHouseProps<string>> = ({ onChange, value, onBlur =
       >
         <div className={classnames(prefixCls + '-search')}>
           <Radio.Group value={iconType} onChange={handleTypeChange}>
-            {
-              iconTypes.map(type => <Radio.Button key={type} value={type}>{type}</Radio.Button>)
-            }
+            {iconTypes.map(type => (
+              <Radio.Button key={type} value={type}>
+                {type}
+              </Radio.Button>
+            ))}
           </Radio.Group>
-          <div style={{ flex: 1, marginLeft: 10, }}>
-            <Input edit={EditStatus.EDIT} value={text} onChange={settext} placeholder={tr('搜索')} allowClear />
+          <div style={{ flex: 1, marginLeft: 10 }}>
+            <Input
+              edit={EditStatus.EDIT}
+              value={text}
+              onChange={settext}
+              placeholder={tr('搜索')}
+              allowClear
+            />
           </div>
         </div>
         <div className={classnames(prefixCls + '-scroll')}>
-          {iconsWithFilter.length ? null : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={tr('没有匹配图标')} style={{ margin: '30px auto 0' }} />}
+          {iconsWithFilter.length ? null : (
+            <Empty
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+              description={tr('没有匹配图标')}
+              style={{ margin: '30px auto 0' }}
+            />
+          )}
           <div className={classnames(prefixCls + '-content')}>
-            {
-              iconsWithFilter.map(id => (
-                <div className={prefixCls + '-content-item'} title={id} key={id} onClick={() => onSelectIcon(id)}>
-                  <Icon type={id} className={prefixCls + '-content-item-iconitem'} />
-                  <div style={{ width: '100%' }}>{id}</div>
-                </div>
-              ))
-            }
+            {iconsWithFilter.map(id => (
+              <div
+                className={prefixCls + '-content-item'}
+                title={id}
+                key={id}
+                onClick={() => onSelectIcon(id)}
+              >
+                <Icon perfix={perfix} type={id} className={prefixCls + '-content-item-iconitem'} />
+                <div style={{ width: '100%' }}>{id}</div>
+              </div>
+            ))}
           </div>
         </div>
       </Drawer>
     </div>
-  )
-}
+  );
+};
 
 interface IconSelectorCmp {
-  (props: IconSelectorProps<string>): React.ReactElement,
-  updateFromIconfontCN: (config: CustomIconOptions) => void,
-  Ant: IconComponent<IconProps>
+  (props: IconSelectorProps<string>): React.ReactElement;
+  updateFromIconfontCN: (config: CustomIconOptions) => void;
+  Ant: IconComponent<IconProps>;
 }
 
 const IconSelector = compose(
   toClass,
   defaultProps(defaultprops),
   withProps(({ allowEdit, edit, style = {}, type, value, onChange }) => {
-    const cStyle: React.CSSProperties = { ...style }
+    const cStyle: React.CSSProperties = { ...style };
     // 根据是否有value和onChange来确定是否受控
-    const controlMode = !(_.isUndefined(value) || _.isUndefined(onChange))
+    const controlMode = !(_.isUndefined(value) || _.isUndefined(onChange));
     if (!controlMode) {
-      cStyle.display = "inline-block"
+      cStyle.display = 'inline-block';
     }
     return {
       style: cStyle,
       value: value || type,
       allowEdit: controlMode ? allowEdit : false,
-      edit: controlMode ? edit : EditStatus.CANCEL
-    }
+      edit: controlMode ? edit : EditStatus.CANCEL,
+      controlMode,
+    };
   }),
-  withEdit(({ value, style, theme, spin, rotate, component, twoToneColor, controlMode }) => {
-    const element = <Icon type={value} style={style} theme={theme} spin={spin} rotate={rotate} component={component} twoToneColor={twoToneColor} />
-    if (!controlMode) {
-      return element
-    }
-    return value ? element : undefined
-  })
-)(IconHouse) as IconSelectorCmp
+  withEdit(
+    ({ value, style, theme, spin, rotate, component, twoToneColor, controlMode, perfix }) => {
+      const element = (
+        <Icon
+          type={value}
+          perfix={perfix}
+          style={style}
+          theme={theme}
+          spin={spin}
+          rotate={rotate}
+          component={component}
+          twoToneColor={twoToneColor}
+        />
+      );
+      if (!controlMode) {
+        return element;
+      }
+      return value ? element : undefined;
+    },
+  ),
+)(IconHouse) as IconSelectorCmp;
 
-IconSelector.updateFromIconfontCN = updateFromIconfontCN
-IconSelector.Ant = Ant
+IconSelector.updateFromIconfontCN = updateFromIconfontCN;
+IconSelector.Ant = Ant;
 
 export default IconSelector;
 
-export {
-  IconSelector
-}
+export { IconSelector };
