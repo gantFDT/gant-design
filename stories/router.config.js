b88ee5ccc1645b41425145de947230da9efa2ac0
袁官东 <yuangd@gantsoftware.com>
2020-03-06 11:01:30 +0800

    config(router): 隐藏不太好的组件，不暴露给用户

--- stories/router.config.js
+++ stories/router.config.js
@@ -1,216 +1,231 @@
-export default [{
-  name: "GantD|开始",
-  children: [{
-    name: 'gantd',
-    nameZh: '开始使用',
-    home: true
-  }]
-}, {
-  name: "组件|数据单元",
-  children: [{
-    name: 'RW',
-    nameZh: '读写分离'
+export default [
+  {
+    name: 'GantD|开始',
+    children: [
+      {
+        name: 'gantd',
+        nameZh: '开始使用',
+        home: true,
+      },
+    ],
   },
   {
-    name: 'Input',
-    nameZh: '文本输入框'
-  },
-  {
-    name: 'InputCellPhone',
-    nameZh: '手机号输入框'
-  },
-  {
-    name: 'InputTelephone',
-    nameZh: '电话输入框'
-  },
-  {
-    name: 'InputUrl',
-    nameZh: '链接输入框'
-  },
-  {
-    name: 'InputEmail',
-    nameZh: '邮箱输入框'
-  },
-  {
-    name: 'InputLanguage',
-    nameZh: '多语言文本输入框'
-  },
-  {
-    name: 'InputNumber',
-    nameZh: '数字输入框'
-  },
-  {
-    name: 'InputMoney',
-    nameZh: '金额输入框'
-  },
-  {
-    name: 'Selector',
-    nameZh: '基础选择器'
-  },
-  {
-    name: 'LocationSelector',
-    nameZh: '地址选择器'
-  },
-  {
-    name: 'Datepicker',
-    nameZh: '日期选择器'
-  },
-  {
-    name: 'ColorPicker',
-    nameZh: '颜色选择器',
-    package: 'color-picker-g',
-  },
-  {
-    name: 'Icon',
-    nameZh: '图标选择器',
-  },
-  ]
+    name: '组件|数据单元',
+    children: [
+      {
+        name: 'RW',
+        nameZh: '读写分离',
+      },
+      {
+        name: 'Input',
+        nameZh: '文本输入框',
+      },
+      {
+        name: 'InputCellPhone',
+        nameZh: '手机号输入框',
+      },
+      {
+        name: 'InputTelephone',
+        nameZh: '电话输入框',
+      },
+      {
+        name: 'InputUrl',
+        nameZh: '链接输入框',
+      },
+      {
+        name: 'InputEmail',
+        nameZh: '邮箱输入框',
+      },
+      {
+        name: 'InputLanguage',
+        nameZh: '多语言文本输入框',
+      },
+      {
+        name: 'InputNumber',
+        nameZh: '数字输入框',
+      },
+      {
+        name: 'InputMoney',
+        nameZh: '金额输入框',
+      },
+      {
+        name: 'Selector',
+        nameZh: '基础选择器',
+      },
+      {
+        name: 'LocationSelector',
+        nameZh: '地址选择器',
+      },
+      {
+        name: 'Datepicker',
+        nameZh: '日期选择器',
+      },
+      {
+        name: 'ColorPicker',
+        nameZh: '颜色选择器',
+        package: 'color-picker-g',
+      },
+      {
+        name: 'Icon',
+        nameZh: '图标选择器',
+      },
+    ],
 
-  // }, {
-  // 	name: "components|Compose 高阶组件",
-  // 	children: [
-  // 		{
-  // 			name: 'anchor',
-  // 			nameZh: '锚点'
-  // 		}
-  // 	]
-  // }, {
-  // 	name: "components|Specific 特殊",
-  // 	children: [
-  // 		,
-  // 		{
-  // 			name: "FiledSchema",
-  // 			nameZh: "属性配置"
-  // 		},
-  // 		{
-  // 			name: "Copy",
-  // 			nameZh: "剪切板"
-  // 		}
-  // 	]
-},
-{
-  name: "组件| 表单",
-  children: [
-    {
-      name: 'SchemaForm',
-      nameZh: '数据驱动表单',
-      package: 'schema-form-g'
-    },
-  ]
-}, {
-  name: "组件| 列表",
-  children: [
-    {
-      name: 'Table',
-      nameZh: '表格',
-      package: 'table-g',
-    },
-    {
-      name: 'SmartTable',
-      nameZh: '智能表格',
-      package: 'smart-table-g',
-    },
-  ]
-}, {
-  name: "组件|容器",
-  children: [{
-    name: 'Header',
-    nameZh: '标头',
-    package: 'header-g',
-  },
-  {
-    name: 'SubMenu',
-    nameZh: '子菜单',
-    package: 'submenu-g',
-  },
-  {
-    name: 'Anchor',
-    nameZh: '锚点',
-    package: 'anchor-g',
-  },
-  {
-    name: 'Modal',
-    nameZh: '弹窗',
-    package: 'modal-g',
-  }, {
-    name: 'Toolbar',
-    nameZh: '工具条',
-  }
-    // {
-    // 	name: 'OverflowTool',
-    // 	nameZh: '子菜单',
-    // 	package: 'overflow-tool-g',
-    // 	nameZh: '溢出工具组',
-    // 	package: 'overflow-tool-g',
-    // },
+    // }, {
+    // 	name: "components|Compose 高阶组件",
+    // 	children: [
     // 		{
-    // 			name: 'FooterToolbar',
-    // 			nameZh: '底部固定工具栏'
+    // 			name: 'anchor',
+    // 			nameZh: '锚点'
     // 		}
-  ]
-}, {
-  name: '组件|其他',
-  children: [
-    //   {
-    //   name: 'Icon',
-    //   nameZh: '图标'
-    // },
-    {
-      name: 'AutoReload',
-      nameZh: '自动刷新组件',
-      package: 'auto-reload-g',
-    }, {
-      name: 'Intro',
-      nameZh: '简介'
-    },
-    {
-      name: 'TaskBoard',
-      nameZh: '任务面板'
-    },
-    {
-      name: 'Loading',
-      nameZh: '载入中'
-    }, {
-      name: 'Style',
-      nameZh: '公共样式类'
-    }, {
-      name: 'Utils',
-      nameZh: '公共工具类'
-    }, {
-      name: 'NodeUtils',
-      nameZh: '公共工具类'
-    }
-    // {
-    // 			name: 'TaskBoard',
-    // 			nameZh: '任务面板'
+    // 	]
+    // }, {
+    // 	name: "components|Specific 特殊",
+    // 	children: [
+    // 		,
+    // 		{
+    // 			name: "FiledSchema",
+    // 			nameZh: "属性配置"
+    // 		},
+    // 		{
+    // 			name: "Copy",
+    // 			nameZh: "剪切板"
     // 		}
-    // {
-    // 	name: 'Exception',
-    // 	nameZh: '异常显现'
-    // },
-    // {
-    // 	name: 'PageLoading',
-    // 	nameZh: '页面加载显示'
-    // },
-    // {
-    // 	name: 'ProfileCard',
-    // 	nameZh: '档案卡片'
-    // }]
-  ]
-}, {
-  name: '组件|实验',
-  children: [
-    //   {
-    //   name: 'Copy',
-    //   nameZh: '复制行数据'
-    // }
-  ]
-}, 
-// {
-//   name: '组件|废弃',
-//   children: [{
-//     name: 'VisibleMenu',
-//     nameZh: '动态列'
-//   }]
-// }
-]
+    // 	]
+  },
+  {
+    name: '组件| 表单',
+    children: [
+      {
+        name: 'SchemaForm',
+        nameZh: '数据驱动表单',
+        package: 'schema-form-g',
+      },
+    ],
+  },
+  {
+    name: '组件| 列表',
+    children: [
+      {
+        name: 'Table',
+        nameZh: '表格',
+        package: 'table-g',
+      },
+      {
+        name: 'SmartTable',
+        nameZh: '智能表格',
+        package: 'smart-table-g',
+      },
+    ],
+  },
+  {
+    name: '组件|容器',
+    children: [
+      {
+        name: 'Header',
+        nameZh: '标头',
+        package: 'header-g',
+      },
+      {
+        name: 'SubMenu',
+        nameZh: '子菜单',
+        package: 'submenu-g',
+      },
+      {
+        name: 'Anchor',
+        nameZh: '锚点',
+        package: 'anchor-g',
+      },
+      {
+        name: 'Modal',
+        nameZh: '弹窗',
+        package: 'modal-g',
+      },
+      {
+        name: 'Toolbar',
+        nameZh: '工具条',
+      },
+      // {
+      // 	name: 'OverflowTool',
+      // 	nameZh: '子菜单',
+      // 	package: 'overflow-tool-g',
+      // 	nameZh: '溢出工具组',
+      // 	package: 'overflow-tool-g',
+      // },
+      // 		{
+      // 			name: 'FooterToolbar',
+      // 			nameZh: '底部固定工具栏'
+      // 		}
+    ],
+  },
+  {
+    name: '组件|其他',
+    children: [
+      //   {
+      //   name: 'Icon',
+      //   nameZh: '图标'
+      // },
+      {
+        name: 'AutoReload',
+        nameZh: '自动刷新组件',
+        package: 'auto-reload-g',
+      },
+      // {
+      //   name: 'Intro',
+      //   nameZh: '简介'
+      // },
+      {
+        name: 'TaskBoard',
+        nameZh: '任务面板',
+      },
+      // {
+      //   name: 'Loading',
+      //   nameZh: '载入中'
+      // },
+      {
+        name: 'Style',
+        nameZh: '公共样式类',
+      },
+      {
+        name: 'Utils',
+        nameZh: '公共工具类',
+      },
+      {
+        name: 'NodeUtils',
+        nameZh: '公共工具类',
+      },
+      // {
+      // 			name: 'TaskBoard',
+      // 			nameZh: '任务面板'
+      // 		}
+      // {
+      // 	name: 'Exception',
+      // 	nameZh: '异常显现'
+      // },
+      // {
+      // 	name: 'PageLoading',
+      // 	nameZh: '页面加载显示'
+      // },
+      // {
+      // 	name: 'ProfileCard',
+      // 	nameZh: '档案卡片'
+      // }]
+    ],
+  },
+  {
+    name: '组件|实验',
+    children: [
+      //   {
+      //   name: 'Copy',
+      //   nameZh: '复制行数据'
+      // }
+    ],
+  },
+  // {
+  //   name: '组件|废弃',
+  //   children: [{
+  //     name: 'VisibleMenu',
+  //     nameZh: '动态列'
+  //   }]
+  // }
+];
