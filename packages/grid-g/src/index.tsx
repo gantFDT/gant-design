114a55c894dba31dda37f8bc7b7bc4d7a3e7d075
chenyl <chenyl@gantsoftware.com>
2020-04-01 10:07:51 +0800

    feat(grid): 添加grid组件基础API及接口

--- packages/grid-g/src/index.tsx
+++ packages/grid-g/src/index.tsx
@@ -1,11 +1,187 @@
-import React from 'react'
+import React, { useState, useCallback, useRef, useMemo } from 'react';
+import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
+import { ColDef, ColGroupDef, GridApi, GridOptions, ColumnApi, GridReadyEvent } from "ag-grid-community";
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+// import "ag-grid-enterprise"
 
-const Index = () => {
+import Header from '@header';
+import { PartRequired, ProtoExtends } from "@util/type"
+import { mapColumns } from './utils'
+
+const defaultProps = {
+    resizable: true,
+    editable: false,
+    filter: true,
+    // 禁止调整列顺序
+    lockPosition: true,
+    // 直接在列头下面显示过滤器
+    floatingFilter: false,
+    // 分页
+    pagination: true,
+}
+
+export enum Filter {
+    Number = "agNumberColumnFilter",
+    Text = 'agTextColumnFilter',
+    Date = "agDateColumnFilter"
+}
+
+export type EditActions = (manage: object, keys: Array<string>) => React.ReactElement
+
+export type OnReady = (api: GridApi) => void
+
+// export type Columns = ColDef
+export type Columns<T extends {} = {}> = {
+    title: React.ReactNode,
+    dataIndex: string,
+    render?: (record: T) => React.ReactNode,
+    children?: Columns<T>[],
+    width?: React.ReactText,
+    checkboxSelection?: boolean,
+    sortable?: boolean,
+    filter?: Filter,
+    hide?: boolean
+}
+
+interface Props<T> {
+    headerProps: {
+        extra?: React.ReactNode,
+        [key: string]: any
+    },
+    editActions: EditActions,
+    columns: Columns<T>[],
+    dataSource: T[],
+    onReady: OnReady
+}
+
+type CustomProps<T> = ProtoExtends<typeof defaultProps, Props<T>>
+
+export type GridProps<T> = CustomProps<T>
+
+// export type GridProps<T> = ProtoExtends<AgGridReactProps, CustomProps<T>>
+
+
+export type GridPropsPartial<T> = PartRequired<GridProps<T>, "columns" | "dataSource">
+
+
+const Grid = function Grid<T>(props: GridPropsPartial<T>) {
+
+    const { headerProps, editActions, onReady, columns: columnDefs, editable } = props
+
+    const apiRef = useRef<GridApi>()
+
+    const columnsRef = useRef<ColumnApi>()
+
+    const onGridReady = useCallback(
+        (params: GridReadyEvent) => {
+            apiRef.current = params.api
+            columnsRef.current = params.columnApi
+            onReady && onReady(params.api)
+            params.api.sizeColumnsToFit()
+        },
+        [onReady],
+    )
+
+    const columns = useMemo<ColDef[] | ColGroupDef[]>(() => mapColumns<T>(columnDefs, editable), [columnDefs, editable])
+
+    const gridOptions = useMemo<GridOptions>(() => {
+        const {
+            dataSource: rowData,
+            resizable,
+            filter,
+            lockPosition,
+            floatingFilter,
+            pagination,
+        } = props
+        return {
+            rowData,
+            columnDefs: columns,
+            onGridReady,
+            floatingFilter,
+            pagination,
+            paginationAutoPageSize: true,
+            onRowEditingStopped: (e) => {
+                console.log(e.data)
+            },
+            defaultColDef: {
+                resizable,
+                filter: floatingFilter ? false : filter,
+                lockPosition
+            }
+        }
+    }, [props, onGridReady, columns])
+
+    const [autoGroupColumnDef, setautoGroupColumnDef] = useState({
+        headerName: "Model",
+        field: 'model',
+        cellRenderer: "agGroupCellRenderer",
+        cellRendererParams: {
+            checkbox: true,
+        }
+    })
+
+    const actions = useMemo(() => {
+        if (editActions) {
+            return editActions({}, [])
+        }
+        return undefined
+    }, [editActions])
+
+    const mergedHeaderProps = useMemo(() => {
+        if (headerProps) {
+            const props = headerProps || {}
+            if (!props.extra) {
+                props.extra = actions
+            } else {
+                props.extra = (
+                    <>
+                        {actions}
+                        {headerProps.extra}
+                    </>
+                )
+            }
+            return props
+        }
+        return undefined
+    }, [actions, headerProps])
+
+    const header = useMemo(() => {
+        if (mergedHeaderProps) {
+            return (
+                <Header {...mergedHeaderProps} />
+            )
+        }
+        return undefined
+    }, [mergedHeaderProps])
+
     return (
-        <>
-            123
-        </>
+        <div className="ag-theme-balham" style={{ width: 600, height: 320 }}>
+            {header}
+            <AgGridReact
+                gridOptions={gridOptions}
+            // rowSelection="multiple"
+            // animateRows
+            // onGridReady={param => ref.current = param.api}
+            // autoGroupColumnDef={autoGroupColumnDef}
+            // groupSelectsChildren
+            // componentWrappingElement="span"
+            //  // 行拖拽
+            // rowDragManaged
+            // suppressRowDrag
+            // defaultColDef={{
+            //     sortable: true,
+            //     filter: true,
+            //     // headerComponentFramework: "div",
+            //     headerComponentParams: {
+            //         name: '123'
+            //     }
+            // }}
+            />
+        </div>
     )
 }
 
-export default Index
+Grid.defaultProps = defaultProps
+
+export default Grid
--- stories/grid/comps.tsx
+++ stories/grid/comps.tsx
@@ -1,27 +1,82 @@
 
 import CodeDecorator from '../_util/CodeDecorator'
 /*! Start !*/
-// import Gird from '@grid'
-import React from 'react'
-
+import React, { useMemo, useEffect, useCallback, useState } from 'react'
+import Gird, { Columns, Filter } from '@grid'
+import { Button } from "antd"
 
 /*! Split !*/
-// const BasicUse = () => {
-//     return (
-//         <Gird />
-//     )
-// }
+const BasicUse = () => {
+
+    const [columns, setcolumns] = useState<Columns[]>([
+        {
+            title: '姓名',
+            dataIndex: "name",
+            checkboxSelection: true,
+            width: 200,
+        },
+        {
+            title: '年龄',
+            dataIndex: "age",
+            sortable: true,
+            filter: Filter.Number,
+            width: 100
+        },
+        {
+            title: '余额',
+            dataIndex: "p",
+            width: 100,
+            hide: true
+        },
+    ])
+
+
+    const [dataSource, setdataSource] = useState(
+        [
+            {
+                name: "里斯",
+                age: 123
+            },
+            {
+                name: "阿斯u",
+                age: 544
+            },
+            {
+                name: "埃斯珀蒂就",
+                age: 1
+            },
+            {
+                name: "撒旦",
+                age: 45
+            },
+        ]
+    )
+
+    const get = useCallback((e) => {
+
+    }, [])
+
+    const header = useMemo(() => ({
+        extra: (
+            <>
+                <Button onClick={get}>获取</Button>
+            </>
+        )
+    }), [])
+    return (
+        <Gird headerProps={header} columns={columns} dataSource={dataSource} editable />
+    )
+}
 /*! End !*/
 const config = {
     codes: [],
-    inline: true,
     useage: '',
     children: [
-        // {
-        //     title: "基本使用",
-        //     describe: "基本使用",
-        //     cmp: BasicUse
-        // }
+        {
+            title: "基本使用",
+            describe: "基本使用",
+            cmp: BasicUse
+        }
     ]
 }
 
