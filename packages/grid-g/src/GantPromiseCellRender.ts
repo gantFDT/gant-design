9893cf8f51c4702e4ccc3b15eb9041e4270d034c
vveyZhang <vvey@vveys-MacBook-Pro.local>
2020-04-21 14:23:23 +0800

    fea(gird) : cellRenderer

--- packages/grid-g/src/GantPromiseCellRender.ts
+++ packages/grid-g/src/GantPromiseCellRender.ts
@@ -1,13 +1,13 @@
-function GantPromiseCellRender() {
-	
- }
-GantPromiseCellRender.prototype.init = function (params) {
-		this.eGui= document.createElement('span');
-		this.params=params;
-		this.renderPromise&&this.renderPromise()
+function GantPromiseCellRender() {}
+GantPromiseCellRender.prototype.init = function(params) {
+  this.params = params;
+  const synchroName = typeof this.renderSynchro === 'function' ? this.renderSynchro() : undefined;
+  this.eGui = document.createElement('span');
+  if (synchroName) this.eGui.innerHTML = synchroName;
+  if (!synchroName && typeof this.renderPromise === 'function') this.renderPromise();
 };
-GantPromiseCellRender.prototype.getGui = function () {
-		return this.eGui;
+GantPromiseCellRender.prototype.getGui = function() {
+  return this.eGui;
 };
 
-export default GantPromiseCellRender
+export default GantPromiseCellRender;
