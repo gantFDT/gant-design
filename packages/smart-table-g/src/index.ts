cf1eb7917e8089187b2bfeb5ed9f0ec0afdbcbf7
yuanguandong <995749721@qq.com>
2021-07-21 16:38:00 +0800

    chore: update

--- packages/smart-table-g/src/index.ts
+++ packages/smart-table-g/src/index.ts
@@ -1,7 +1,8 @@
-import SmartTable from './SmartTable';
 import { setFields } from './formatschema';
+import SmartTable from './SmartTable';
 
-export { default as TableConfig } from './config/UIContent'
+export { default as TableConfig } from './config/UIContent';
+
 
 SmartTable.setFields = setFields;
 export default SmartTable;
