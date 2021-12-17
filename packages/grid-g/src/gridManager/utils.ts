13d460d21455abd9f84cce4a83d4813f2077ee2e
vveyZhang <helloxwz@qq.com>
2021-12-17 18:20:13 +0800

    清除console

--- packages/grid-g/src/gridManager/utils.ts
+++ packages/grid-g/src/gridManager/utils.ts
@@ -75,12 +75,10 @@ export const isEqualObj = (obj, obj2) => {
   let _EqualObj = true;
   if (!isPlainObject(obj) || !isPlainObject(obj2)) {
     if (isEmptyObj(obj) && isEmptyObj(obj2)) return true;
-    if (!isEqual(obj, obj2)) console.log('--->', obj, obj2);
     return isEqual(obj, obj2);
   }
   if (isNull(obj) && isNull(obj2)) return true;
   if (isNull(obj) || isNull(obj2)) {
-    console.log('--->', obj, obj2);
     return false;
   }
   const newObj = { ...obj, ...obj2 };
@@ -102,7 +100,6 @@ export const isEqualObj = (obj, obj2) => {
       }
     }
     if (!_EqualObj) {
-      console.log('---->', value1, value2,i);
       return _EqualObj;
     }
   }
