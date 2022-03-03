f18abd95fb75628ed4b433c940296f3ce26c956d
Favori <995749721@qq.com>
2022-03-03 10:29:05 +0800

    chore: udpate

--- packages/gantd/src/button/index.tsx
+++ packages/gantd/src/button/index.tsx
@@ -14,6 +14,6 @@ const Button = (buttonProps: GantButtonProps) => {
   const props = { ...globalConfig, ...buttonProps };
   return <AntButton {...props}></AntButton>;
 };
-
+Button.Group = AntButton.Group
 export {setGlobalConfig} from './utils'
 export default Button;
