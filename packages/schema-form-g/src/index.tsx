be28d06ee173aa1b8bac585ec564ea787210cac9
vveyZhang <helloxwz@qq.com>
2022-12-27 21:42:56 +0800

    fix:

--- packages/schema-form-g/src/index.tsx
+++ packages/schema-form-g/src/index.tsx
@@ -28,9 +28,10 @@ class SchemaForm extends React.Component<Props, { schemaCount: number }> {
     frameworkComponents: {},
   };
 
-  
+
   /**收集所有子级节点的初始数据 */
   initialValueMap = new Map();
+  
   constructor(props) {
     super(props);
     const { schema } = props;
