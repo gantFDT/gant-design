b1331a0337f0379470313173ba21b4961cbbde7d
vveyZhang <helloxwz@qq.com>
2023-02-24 15:20:14 +0800

    fix:grid优化

--- packages/grid-g/src/GridEidtColumn.tsx
+++ packages/grid-g/src/GridEidtColumn.tsx
@@ -135,6 +135,7 @@ export default WrapperComponent =>
             return false;
           },
           getValue: () => {
+            let nodeValue = get(node, `data.${field}`);
             const value = initValueFormatter
               ? initValueFormatter({
                   ...props,
