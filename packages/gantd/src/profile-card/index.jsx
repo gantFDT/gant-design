1a659f19f17c1dfde55d61693c33c800a4aa97a6
张伟 <helloxwz@qq.com>
2023-04-13 17:18:54 +0800

    fix:补全国际化

--- packages/gantd/src/profile-card/index.jsx
+++ packages/gantd/src/profile-card/index.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, Form, Tag, Tooltip, Popover } from 'antd';
-import classnames from 'classnames'
-import { ConfigConsumer } from '../config-provider'
-import { Icon } from '@data-cell'
+import classnames from 'classnames';
+import { ConfigConsumer } from '../config-provider';
+import { Icon } from '@data-cell';
 
 class Item extends Component {
   render() {
-    const { value, mode } = this.props
-    return <>
-      {
-        mode === 'tags' ?
-          value.map(v => <Tag key={v}>{v}</Tag>) :
+    const { value, mode } = this.props;
+    return (
+      <>
+        {mode === 'tags' ? (
+          value.map(v => <Tag key={v}>{v}</Tag>)
+        ) : (
           <Tooltip placement="bottomLeft" title={value}>
-            <p className="itemText omitdisplay" style={{ color: value ? 'rgba(0,0,0,.9)' : '#ccc' }}>{value || '暂无'}</p>
+            <p
+              className="itemText omitdisplay"
+              style={{ color: value ? 'rgba(0,0,0,.9)' : '#ccc' }}
+            >
+              {value}
+            </p>
           </Tooltip>
-      }
-    </>
+        )}
+      </>
+    );
   }
 }
 
@@ -36,13 +43,13 @@ class ProfileCard extends Component {
     backgroundBlur: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
     layout: PropTypes.shape({
       labelCol: PropTypes.object,
-      wrapperCol: PropTypes.object
+      wrapperCol: PropTypes.object,
     }),
     trigger: PropTypes.oneOf(['hover', 'click']),
     placement: PropTypes.string,
     overlayClassName: PropTypes.string,
-    onAvatarClick: PropTypes.func
-  }
+    onAvatarClick: PropTypes.func,
+  };
 
   static defaultProps = {
     labelAlign: 'default',
@@ -59,38 +66,32 @@ class ProfileCard extends Component {
         sm: { span: 12 },
       },
     },
-    trigger: 'hover'
-  }
+    trigger: 'hover',
+  };
 
-
-  getField = (fields) => {
-    const { form: { getFieldDecorator }, layout, data } = this.props;
-    return (edit) => (
+  getField = fields => {
+    const {
+      form: { getFieldDecorator },
+      layout,
+      data,
+    } = this.props;
+    return edit => (
       <>
         <Row gutter={24} style={{ marginBottom: '10px', width: '100%' }}>
-          {
-            fields.map(field => (
-              <Col key={field.key} style={{minWidth:200}}>
-                <Form.Item
-                  label={field.label}
-                  {...layout}
-                >
-                  {
-                    getFieldDecorator(field.key, {
-                      initialValue: data[field.key] || '',
-                      ...field.options
-                    })(
-                      <Item {...field} />
-                    )
-                  }
-                </Form.Item>
-              </Col>
-            ))
-          }
+          {fields.map(field => (
+            <Col key={field.key} style={{ minWidth: 200 }}>
+              <Form.Item label={field.label} {...layout}>
+                {getFieldDecorator(field.key, {
+                  initialValue: data[field.key] || '',
+                  ...field.options,
+                })(<Item {...field} />)}
+              </Form.Item>
+            </Col>
+          ))}
         </Row>
       </>
-    )
-  }
+    );
+  };
 
   renderWithConfigConsumer = ({ getPrefixCls }) => {
     const {
@@ -111,52 +112,78 @@ class ProfileCard extends Component {
       className,
       placement = 'bottom',
       overlayClassName,
-      onAvatarClick
+      onAvatarClick,
     } = this.props;
     const prefixCls = getPrefixCls('profilecard', customizePrefixCls);
     const content = (
-      <div className={classnames(prefixCls, avatarAlign, className)} style={{ height: height || 'auto', paddingBottom: extraBottom && 40 }}>
-        {extraLeftTop && (
-          <div className={`${prefixCls}-fixedlefttop`}>{extraLeftTop}</div>
-        )}
-        {extraRightTop && (
-          <div className={`${prefixCls}-fixedrighttop`}>{extraRightTop}</div>
-        )}
-        {showAvatar &&
+      <div
+        className={classnames(prefixCls, avatarAlign, className)}
+        style={{ height: height || 'auto', paddingBottom: extraBottom && 40 }}
+      >
+        {extraLeftTop && <div className={`${prefixCls}-fixedlefttop`}>{extraLeftTop}</div>}
+        {extraRightTop && <div className={`${prefixCls}-fixedrighttop`}>{extraRightTop}</div>}
+        {showAvatar && (
           <div className={classnames(`${prefixCls}-avatarBox`, avatarAlign)}>
-            <div className={classnames(`${prefixCls}-avatarBox-bg`, avatarAlign)} style={backgroundImage === false ? {} : { backgroundImage: `url(${backgroundImage || data.avatarUrl})`, backgroundSize: 'cover', filter: `blur(${backgroundBlur}px)` }}></div>
-            <div className={classnames(`${prefixCls}-avatarBox-avatar`, avatarAlign)} style={{ backgroundImage: `url(${data.avatarUrl})`, cursor: onAvatarClick ? 'pointer' : 'default' }} onClick={onAvatarClick && onAvatarClick}>
+            <div
+              className={classnames(`${prefixCls}-avatarBox-bg`, avatarAlign)}
+              style={
+                backgroundImage === false
+                  ? {}
+                  : {
+                      backgroundImage: `url(${backgroundImage || data.avatarUrl})`,
+                      backgroundSize: 'cover',
+                      filter: `blur(${backgroundBlur}px)`,
+                    }
+              }
+            ></div>
+            <div
+              className={classnames(`${prefixCls}-avatarBox-avatar`, avatarAlign)}
+              style={{
+                backgroundImage: `url(${data.avatarUrl})`,
+                cursor: onAvatarClick ? 'pointer' : 'default',
+              }}
+              onClick={onAvatarClick && onAvatarClick}
+            >
               {!data.avatarUrl && <Icon type="user" />}
             </div>
           </div>
-        }
-        <div className={classnames(`${prefixCls}-fields`, labelAlign == 'left' && 'dataform', avatarAlign)} style={{ width: children ? 'auto' : 'auto' }}>
-          <Form labelAlign={labelAlign}>
-            {this.getField(fields)(false)}
-          </Form>
-        </div>
-        {extraBottom && (
-          <div className={`${prefixCls}-fixedbottom`}>{extraBottom}</div>
         )}
+        <div
+          className={classnames(
+            `${prefixCls}-fields`,
+            labelAlign == 'left' && 'dataform',
+            avatarAlign,
+          )}
+          style={{ width: children ? 'auto' : 'auto' }}
+        >
+          <Form labelAlign={labelAlign}>{this.getField(fields)(false)}</Form>
+        </div>
+        {extraBottom && <div className={`${prefixCls}-fixedbottom`}>{extraBottom}</div>}
       </div>
-    )
+    );
 
     return (
-      data && (children ?
-        (
-          <Popover placement={placement} content={content} trigger={trigger} overlayClassName={overlayClassName}>
-            {children}
-          </Popover>
-        ) : content
-      )
-    )
-  }
+      data &&
+      (children ? (
+        <Popover
+          placement={placement}
+          content={content}
+          trigger={trigger}
+          overlayClassName={overlayClassName}
+        >
+          {children}
+        </Popover>
+      ) : (
+        content
+      ))
+    );
+  };
 
   render() {
-    return <ConfigConsumer>{this.renderWithConfigConsumer}</ConfigConsumer>
+    return <ConfigConsumer>{this.renderWithConfigConsumer}</ConfigConsumer>;
   }
 }
 
-// export 
+// export
 
-export default ProfileCard
+export default ProfileCard;
