54dabed50370473c3be614bc08683bf01e3ce130
zoushuai <zoushuai@gantsoftware.com>
2024-09-14 13:49:31 +0800

    feat: grid表格修改右键上下文菜默认下拉项国际化调整

--- packages/grid-g/src/locale/zh-CN.ts
+++ packages/grid-g/src/locale/zh-CN.ts
@@ -244,5 +244,7 @@ export default {
   exportSelected: '导出选中行',
   selectedData: '已选中数据',
   clear: '清除',
-  outOfLimit: '当前总数超过'
+  outOfLimit: '当前总数超过',
+  exportDataAsExcel: '全部导出',
+  exportSelectedDataAsExcel: '选中导出'
 };
